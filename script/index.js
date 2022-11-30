7e6dedda2889cb6005641dbb7a615faa02bdda14
Anoai100 <anoainyts100@gmail.com>
2022-12-01 01:51:16 +0400

    Add a validation function for user input
    This ensures that the script only takes one of the three valid options

--- script/index.js
+++ script/index.js
@@ -1,6 +1,21 @@
-const userChoice = prompt("Hi, your turn");
-let playerSelection = userChoice.toLowerCase();
+const x = prompt("Hi player, please enter your choices");
+const userChoice = x.toLowerCase();
 
+function validate(word) {
+    const choices = ["rock", "paper", "scissors"];
+
+    if(choices.includes(word)) {
+        return(word)
+    }
+
+    else {
+        const correct = prompt("rock, paper or scissors");
+        return(validate(correct));
+    }
+}
+
+const playerSelection = validate(userChoice);
+console.log(playerSelection);
 
 function getComputerChoice () {
 
@@ -23,19 +38,42 @@ let computerSelection = getComputerChoice();
 
 function playRound(master, sleeve) {
 
+    const output = "";
+
     if(master[0] === "p" && sleeve[0] === "s") {
-        return(`You lose, ${sleeve} beats ${master}`);
+        output.concat(`You lose, ${sleeve} beats ${master}!`);
     }
 
     else if(master[0] < sleeve[0]) {
-        return(`You win, ${master} beats ${sleeve}`);
+        output.concat(`You win, ${master} beats ${sleeve}!`);
+    }
+
+    else if(master[0] === sleeve[0]) {
+        output.concat(`Draw, you chose the same!`)
     }
 
     else {
-        return(`You lose,  ${sleeve} beats ${master}`);
+        output.concat(`You lose,  ${sleeve} beats ${master}!`);
     }
+    return(output);
 };
 
 console.log(playRound(playerSelection, computerSelection));
 
-    
+/*
+function game() {
+    const roundOf = 5;
+    let result = roundOf
+    for(let i = roundOf; i >= 1; i--) {
+        console.log(playRound(playerSelection, computerSelection));
+        const check = playRound(playerSelection, computerSelection);
+        choose();
+
+        if("lose" in check || "Draw" in check) {
+            result -= 1;
+        }
+    }
+    return(result);
+    console.log(result);
+};
+*/
