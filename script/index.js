f977a7b1e6f8ab28638315ba561aa172d6a1de9d
Anoai100 <anoainyts100@gmail.com>
2022-12-07 20:50:21 +0400

    Update the script to near completion
    The script is not complete yet as there is a something i can't figure
    out with the last function.
    I will finish it with time.

--- script/index.js
+++ script/index.js
@@ -1,96 +1,105 @@
-const x = prompt("Hi player, please enter your choices");
-const userChoice = x.toLowerCase();
+function collection () { 
+  const x = prompt("Hi player, please enter your choices");
+    const userChoice = x.toLowerCase();
 
-function validate(word) {
-    const choices = ["rock", "paper", "scissors"];
+    function validate(word) {
+        const choices = ["rock", "paper", "scissors"];
 
-    if(choices.includes(word)) {
-        return(word)
+        if(choices.includes(word)) {
+            return(word)
+        }
+
+        else {
+            const correct = prompt("rock, paper or scissors");
+            return(validate(correct));
+        }
     }
 
-    else {
-        const correct = prompt("rock, paper or scissors");
-        return(validate(correct));
-    }
+    const playerSelection = validate(userChoice);
+
+    function getComputerChoice () {
+
+        const computerChoice = Math.floor(Math.random() * 9) + 1;
+
+        if(computerChoice <= 3) {
+            return("rock");
+        }
+
+        else if(computerChoice > 3 && computerChoice <= 6) {
+            return("scissors");
+        }
+        
+        else {
+            return("paper")
+        };
+    };
+
+    let computerSelection = getComputerChoice();
+    function playRound(master, sleeve) {
+
+        if(master[0] === "p" && sleeve[0] === "s") {
+            return(`You lose, ${sleeve} beats ${master}!`);
+        }
+
+        else if(master[0] < sleeve[0]) {
+            return(`You win, ${master} beats ${sleeve}!`);
+        }
+
+        else if(master[0] === sleeve[0]) {
+            return(`it's a draw, you chose the same!`);
+        }
+
+        else {
+            return(`You lose, ${sleeve} beats ${master}!`);
+        }
+    };
+
+    const output = playRound(playerSelection, computerSelection);
+    return(output);
 }
 
-const playerSelection = validate(userChoice);
 
-function getComputerChoice () {
-
-    const computerChoice = Math.floor(Math.random() * 9) + 1;
-
-    if(computerChoice <= 3) {
-        return("rock");
-    }
-
-    else if(computerChoice > 3 && computerChoice <= 6) {
-        return("scissors");
-    }
-    
-    else {
-        return("paper")
-    };
-};
-
-let computerSelection = getComputerChoice();
-function playRound(master, sleeve) {
-
-    if(master[0] === "p" && sleeve[0] === "s") {
-        return(`You lose, ${sleeve} beats ${master}!`);
-    }
-
-    else if(master[0] < sleeve[0]) {
-        return(`You win, ${master} beats ${sleeve}!`);
-    }
-
-    else if(master[0] === sleeve[0]) {
-        return(`it's a draw, you chose the same!`);
-    }
-
-    else {
-        return(`You lose, ${sleeve} beats ${master}!`);
-    }
-};
-
-const output = playRound(playerSelection, computerSelection);
-
-
-function game(hero) {
-    const bestOfFive = 5;
+function game(hero, n) {
     let player = 0;
     let computer = 0;
     let tie = 0;
 
-    for(let i = 0; i <= bestOfFive; i++) {
-        if(hero.includes("lose")) {
+    for(let i = 1; i <= n; i++) {
+        function compare(func) {
+            return(func);
+        };
+
+        const test = compare(hero);
+
+        if(test.includes("lose")) {
             computer += 1;
         }
 
-        else if(hero.includes("draw")) {
+        else if(test.includes("draw")) {
             tie += 1;
         }
         
         else {
             player += 1;
         };
+    
 
+        const result = `You:${player}; Draw:${tie}; Computer:${computer}`
+    
+        if(player < computer) {
+            return(`(${result}); Oops! Better luck next time.`);
+        }
+    
+        else if(player > computer) {
+            return(`(${result}); Congratulations!!! You're the Man!`);
+        }
+    
+        else {
+            return(`(${result}); It's a tie, dare to go again`);
+        }
     }
 
-    const result = `You:${player}; Draw:${tie}; Computer:${computer}`
-
-    if(player < computer) {
-        return(`(${result}); Oops! Better luck next time.`);
-    }
-
-    else if(player > computer) {
-        return(`(${result}); Congratulations!!! You're the Man!`);
-    }
-
-    else {
-        return(`(${result}); It's a tie, dare to go again`);
-    }
 };
 
-console.log(game(output));
+console.log(game(collection(), 5));
 
